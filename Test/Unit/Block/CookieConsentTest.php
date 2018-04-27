e0e3868dad89c05683e3cb49a6a340d03af48e03
Marcel Hauri <marcel@hauri.me>
2018-04-27 21:33:54 +0200

    [task] extend tests to cover new classes

--- Test/Unit/Block/CookieConsentTest.php
+++ Test/Unit/Block/CookieConsentTest.php
@@ -14,13 +14,29 @@ final class CookieConsentTest extends AbstractBlockSetup
      * @var CookieConsent
      */
     private $block;
+
     public function setUp()
     {
         parent::setUp();
         $this->block = new CookieConsent($this->context, $this->config, []);
     }
-    public function testIsCookieConsentEnabled()
+    public function testIsEnabled()
     {
-        $this->assertSame(false, $this->block->isCookieConsentEnabled());
+        $this->assertSame(false, $this->block->isEnabled());
+    }
+
+    public function testIsStatic()
+    {
+        $this->assertSame(false, $this->block->isStatic());
+    }
+
+    public function testGetCookiePosition()
+    {
+        $this->assertSame(null, $this->block->getCookiePosition());
+    }
+
+    public function testGetCookieConsentConfiguration()
+    {
+        $this->assertSame('{"palette":{"popup":{"background":"#000"},"button":{"background":"#fff"}},"position":null,"static":false}', $this->block->getCookieConsentConfiguration());
     }
 }
